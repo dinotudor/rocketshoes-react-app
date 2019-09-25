7c9d3972e74ae1f3b72c92a7da023562e643df78
Dino Tudor <dinotudor@gmail.com>
2019-09-25 19:12:19 +0200

    WIP header, added react icons and link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { Container } from './styles';
+import { MdShoppingBasket } from 'react-icons/md';
+
+import { Container, Cart } from './styles';
 import logo from '../../assets/images/logo.svg';
 
 export default function Header() {
   return (
     <Container>
-      <img src={logo} alt="logo rocketshoes" />
+      <Link to="/">
+        <img src={logo} alt="logo rocketshoes" />
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>Meu carrinho</strong>
+          <span>3 Items</span>
+        </div>
+        <MdShoppingBasket size={36} color="#FFF" />
+      </Cart>
     </Container>
   );
 }
