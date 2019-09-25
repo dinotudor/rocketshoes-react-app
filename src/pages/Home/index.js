3e3fd2d16713cdeb5eb2d8ff35f39f0fd4a12c3e
Dino Tudor <dinotudor@gmail.com>
2019-09-25 20:59:02 +0200

    WIP Home CSS

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,82 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
+import tenis from '../../assets/images/tenis.jpg';
 import { ProductList } from './styles';
 
 export default function Home() {
   return (
     <ProductList>
       <li>
-        <img src={} alt="" />
+        <img src={tenis} alt="shoe" />
         <strong>Nice shoe</strong>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+          <span>Add to cart</span>
+        </button>
+      </li>
+
+      <li>
+        <img src={tenis} alt="shoe" />
+        <strong>Nice shoe</strong>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+          <span>Add to cart</span>
+        </button>
+      </li>
+
+      <li>
+        <img src={tenis} alt="shoe" />
+        <strong>Nice shoe</strong>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+          <span>Add to cart</span>
+        </button>
+      </li>
+
+      <li>
+        <img src={tenis} alt="shoe" />
+        <strong>Nice shoe</strong>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+          <span>Add to cart</span>
+        </button>
+      </li>
+
+      <li>
+        <img src={tenis} alt="shoe" />
+        <strong>Nice shoe</strong>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+          <span>Add to cart</span>
+        </button>
+      </li>
+
+      <li>
+        <img src={tenis} alt="shoe" />
+        <strong>Nice shoe</strong>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+          <span>Add to cart</span>
+        </button>
       </li>
     </ProductList>
   );
