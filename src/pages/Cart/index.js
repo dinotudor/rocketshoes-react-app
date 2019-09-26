a70a3dfe1f01ad42461264b4e8154dac58d6a8c3
Dino Tudor <dinotudor@gmail.com>
2019-09-26 11:17:33 +0200

    Cart JSX WIP

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,61 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icon/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <div />;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>Product</th>
+            <th>Quantity</th>
+            <th>Total</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src={tenis} al="tenis" />
+            </td>
+            <td>
+              <strong>Cool tenis</strong>
+              <span>$90,00</span>
+            </td>
+            <td>
+              <button>
+                <MdRemoveCircleOutline size={20} color="#7159c1" />
+              </button>
+              <input type="number" readOnly value={1} />
+              <button>
+                <MdAddCircleOutline size={20} color="#7159c1" />
+              </button>
+            </td>
+            <td>
+              <strong>$ 180,00</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button>Checkout</button>
+        <Total>
+          <span>Total</span>
+          <strong>$212,87</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
